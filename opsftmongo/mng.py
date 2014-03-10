bb4df58c2eb45bb8db38f43df857a7646b416f0f
eggtree <eggtree@requiredhealth.org>
2014-03-09 20:21:25 -0500

    tidy up

--- opsftmongo/mng.py
+++ opsftmongo/mng.py
@@ -3,22 +3,10 @@ import os
 
 
 def index():
-    #print 1
-    #print os.getenv('OPENSHIFT_MONGODB_DB_URL','blah')
-    #print os.environ['MONGO_HOST']
+    # e.g. mongodb://admin:password@127.8.126.130:27017/
     client = MongoClient(os.getenv('MONGO_DB_URL','mongodb://localhost'))
-    # check whether a username/pwd are set and if so
-    # connect with authentication, otherwise connect without
-    # http://docs.mongodb.org/manual/tutorial/enable-authentication/
-    #print 2
     db = client[os.environ['MONGO_DB_NAME']]
-
-   # mongo_user = os.environ.get('MONGO_USER')
-    #mongo_pwd = os.environ.get('MONGO_PWD')
-    #print 3
-    #if mongo_user and mongo_pwd:
-    #    db.authenticate(mongo_user, mongo_pwd)
-    #print 4
+    
     coll = db['issuer']
     print coll.count()
     return coll.count()
