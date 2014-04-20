b1983121903a3bf819e0945d6dfaf2c16895ce07
eggtree <eggtree@requiredhealth.org>
2014-04-19 19:53:04 -0500

    tidy up usage of MONGO_DB_URL
    removed some superfluous comments and added an explaination into
    why its best if users are added to the admin db in mongo

--- opsftmongo/mng.py
+++ opsftmongo/mng.py
@@ -3,9 +3,19 @@ import os
 
 
 def index():
-    # e.g. mongodb://admin:password@127.8.126.130:27017/
+    # when you connect to mongo using a url which includes authentication 
+    # information you have to ensure one of the following is true:
+    # a) the user exists in the admin database
+    # b) the url includes the database where the user exists
+    # e.g. MONGO_DB_URL=mongodb://admin:password@127.8.126.130:27017/acaex
+    # This is because mongo users are per database, so if your user is
+    # not in the admin db you need to pass in at MongoClient initialization
+    # time which db to check for authentication info. 
+    # Normally you can wait and pick your database to connect to after 
+    # MongoClient is initialized
+    # Fortunately openshift stores users in the admin db
     client = MongoClient(os.getenv('MONGO_DB_URL','mongodb://localhost'))
-    db = client[os.environ['MONGO_DB_NAME']]
+    db = client[os.getenv('MONGO_DB_NAME', 'acaex')]
     
     coll = db['issuer']
     print coll.count()
